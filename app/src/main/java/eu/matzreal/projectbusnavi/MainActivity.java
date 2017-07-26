72f47d8f709093ecbbd125323eb5a4052631bebb
Matzreal <smuggler232@gmail.com>
2017-07-26 21:26:10 +0200

    initial TabLayout setup

--- app/src/main/java/eu/matzreal/projectbusnavi/MainActivity.java
+++ app/src/main/java/eu/matzreal/projectbusnavi/MainActivity.java
@@ -1,6 +1,8 @@
 package eu.matzreal.projectbusnavi;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        viewPager.setAdapter(new SimpleFragmentPagerAdapter(getSupportFragmentManager()));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        // Remove the action bar shadow effect on devices with Android 5.0+
+        getSupportActionBar().setElevation(0);
     }
+
 }
---
+++ app/src/main/java/eu/matzreal/projectbusnavi/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,36 @@
+package eu.matzreal.projectbusnavi;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+    private static final int PAGE_COUNT = 2;
+    private static final String[] PAGE_TITLES = {"Route", "Menu"};
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new RouteSearchFragment();
+            case 1:
+                return new NaviMenuFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return PAGE_TITLES[position];
+    }
+}
