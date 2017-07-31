c790569f2b3b72561e8ccd8daede216eb79dd5e3
Matzreal <smuggler232@gmail.com>
2017-07-31 21:54:39 +0200

    initial bus stops dialog setup

--- app/src/main/java/eu/matzreal/projectbusnavi/RouteSearchFragment.java
+++ app/src/main/java/eu/matzreal/projectbusnavi/RouteSearchFragment.java
@@ -3,9 +3,11 @@ package eu.matzreal.projectbusnavi;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class RouteSearchFragment extends Fragment {
 
+    private TextView directionFromTextView;
 
     public RouteSearchFragment() {
         // Required empty public constructor
@@ -23,7 +26,22 @@ public class RouteSearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_route_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_route_search, container, false);
+
+        directionFromTextView = view.findViewById(R.id.tv_direction_from);
+        directionFromTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showBusStopsDialog();
+            }
+        });
+
+        return view;
     }
 
+    private void showBusStopsDialog() {
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        BusStopsFragment fragment = BusStopsFragment.newInstance();
+        fragment.show(fm, "fragment_bus_stops");
+    }
 }
